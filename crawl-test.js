b6a172e06636c7912807546e3ae3fc5588cf1d38
AhmedMakhlouf <ahmedmakhlouf245@gmail.com>
2023-06-02 23:31:00 +0200

    normalizeURL and tests

---
+++ crawl-test.js
@@ -0,0 +1,16 @@
+const { normalizeURL } = require('./crawl.js')
+const { test, expect } = require('@jest/globals')
+
+test('normalizeURL strip protocol', ()=>{
+    const input = ''
+    const actual = normalizeURL(input)
+    const expected = ''
+    expect(actual).toEqual(expected)
+})
+
+test('normalizeURL capitals', ()=>{
+    const input = ''
+    const actual = normalizeURL(input)
+    const expected = ''
+    expect(actual).toEqual(expected)
+})

debb55a897652446e6073b19d7bb8c60392cb24a
AhmedMakhlouf <ahmedmakhlouf245@gmail.com>
2023-06-03 23:51:56 +0200

    crawl single page with error handling

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
-console.log(`Hello world`);
+const { crawlPage } = require('./crawl')
+
+function main(){
+    if(process.env.length < 3){
+        console.log("No Website provided");
+        process.exit(1)
+    }
+    if (process.env.length > 3){
+        console.log(`Too many command line args`);
+    }
+    const baseURL = process.argv[2]
+    console.log(`starting crawl ${baseURL}`);
+
+    crawlPage(baseURL)
+}
+main()
